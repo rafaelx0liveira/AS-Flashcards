93bed28fd9e8bc01131495c1767c785a456cb941
Andre-Queiroz <andretorresdequeiroz@gmail.com>
2022-05-23 21:26:22 -0300

    Initial commit

---
+++ src/test/java/com/pucsp/flashcards/FlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucsp.flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
