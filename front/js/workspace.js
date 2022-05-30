7ef913e9d648db47c704e33c60be11e8ef8353c0
Rafael Oliveira <rafaelaparecido.oliveirasilva@gmail.com>
2022-05-29 23:58:12 -0300

    Adicionando opcoes do flashcard

--- front/js/workspace.js
+++ front/js/workspace.js
@@ -3,3 +3,13 @@ const card = document.querySelector(".card__inner");
 card.addEventListener("click", function (e) {
   card.classList.toggle('is-flipped');
 });
+
+function on_off(div)
+{
+    var divbox = document.getElementById(criar-flashcard);
+
+    if(divbox.style.visibility == 'hidden')
+        divbox.style.visibility = 'visible';
+        else
+          divbox.style.visibility = 'hidden';
+}
