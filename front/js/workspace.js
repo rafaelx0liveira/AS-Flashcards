9dbb01da30e880825bd29a247edab3ba72438692
Rafael Oliveira <rafaelaparecido.oliveirasilva@gmail.com>
2022-05-30 23:50:55 -0300

    Adicionando operacoes do Workspace

--- front/js/workspace.js
+++ front/js/workspace.js
@@ -1,6 +1,7 @@
 const base_url = "http://127.0.0.1:8080/flashcards";
 
 const card = document.querySelector(".card__inner");
+const form = document.getElementById("formulario-flashcard");
 
 card.addEventListener("click", function (e) {
   card.classList.toggle("is-flipped");
@@ -61,9 +62,8 @@ async function createFlashcard() {
   const user_id = readCookie("user_id");
 
   const body_data = {
-    front: "question", // pegar do form
-    back: "answer", // pegar do form
-    description: "description", // pegar do form
+    front: form.elements["pergunta"].value, // pegar do form
+    back: form.elements["resposta"].value, // pegar do form
   };
 
   const options = {
@@ -83,16 +83,52 @@ async function playFlashcard() {
   const flashcard_id = "dc6b29d9-7f90-483d-9e13-630a89d969d5"; // pegar do form
   const url = base_url + "/" + flashcard_id;
 
+  var pick 
+
+  var botaoAcerto = document.getElementById["acertou"].value;
+  var botaoErro = document.getElementById["errou"].value;
+
+  botaoAcerto.disabled = true;
+  botaoErro.disabled = true;
+
+  var controle;
+
+  if (botaoAcerto === true) {
+    controle = true;
+  }
+  else if(botaoErro === false){
+    controle = false;
+  }
+
   const options = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "is-correct": true,
+      "is-correct": controle,
     },
   };
 
   const response = await fetch(url, options);
   const data = await response.json();
+}
+
+function print(valor) {
+  var letra = document.getElementById('acertou').value;
+  alert(letra);
+  }
+
+function disableButtons(){
+  var botaoAcerto = document.getElementById["acertou"].value;
+  var botaoErro = document.getElementById["errou"].value;
+
+  botaoAcerto.disabled = true;
+  botaoErro.disabled = true;
+
+  var valor = botaoAcerto.value; 
+
+  alert(valor);
+  alert(botaoErro.value);
+
 }
 
 async function deleteFlashcard() {
@@ -133,24 +169,9 @@ function on_off(div) {
   }
 }
 
-let userSection = document.querySelector('.acerto-erro');
+let userSection = document.querySelector(".acerto-erro");
 let isShow = true;
 
-// function on_off() {
-//   if(isShow){
-//     userSection.style.display = "block";
-//     isShow = false;
-//   }
-//   else{
-//     userSection.style.display = "none";
-//     isShow = true;
-//   }
-// }
-
 function on_off() {
-  userSection.classList.toggle("hide")
-
+  userSection.classList.toggle("hide");
 }
-
-
-
